5a7532620367c70bbfef113897d591da0bde9b13
lestercch <lester_cch93@mesoamericana.edu.gt>
2022-01-27 23:40:53 -0600

    Adding data tables

--- laravel8crud/resources/views/articulo/index.blade.php
+++ laravel8crud/resources/views/articulo/index.blade.php
@@ -1,6 +1,12 @@
 <!--This is our main page, what we're going to show at the begining-->
 
-@extends('layouts.plantillabase');
+@extends('layouts.plantillabase')
+
+<!--Here we're gonna work with with datatables-->
+@section('css')
+    <link href="https://cdn.datatables.net/1.11.4/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+@endsection
+<!--End of datatables-->
 
 @section('contenido')
 {{--Here we say that when we press Crear button, it will
@@ -9,8 +15,13 @@
 <a href="articulos/create" class="btn btn-primary">Crear</a>  
 
 <!--We're gonna create our table-->
-<table class="table table-dark table-striped mt-4">
-    <thead>
+<!--'id' in table is for datatables-->
+{{--
+    we can use this style 'class="table table-striped" style="width:100%"' or
+    the one mentioned here. Style is indistinct
+    --}}
+<table id="articulos" class="table table-striped table-bordered shadow-lg mt-4" style="width:100%">
+    <thead class="bg-primary text-white">
         <tr>
             <th scope="col">ID</th>
             <th scope="col">Codigo</th>
@@ -44,4 +55,25 @@
         @endforeach
     </tbody>
 </table>
+
+<!--This is for javascript-->
+@section('js')
+
+    <!--These files will add the index pagem search engine, etc. -->
+
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.4/js/dataTables.bootstrap5.min.js"></script>
+
+    <!-- This will initiate our table functions -->
+    <script>
+        $(document).ready(function() {
+            $('#articulos').DataTable({
+                "lengthMenu": [[5, 10, 50, 100, -1], [5, 10, 50, 100, "All"]]
+            });
+        } );
+    </script>
+
+@endsection
+<!--end javascript-->
 @endsection
--- laravel8crud/resources/views/layouts/plantillabase.blade.php
+++ laravel8crud/resources/views/layouts/plantillabase.blade.php
@@ -4,6 +4,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!--We're gonna use datatables here-->
+    <!--IMPORTANT: datatables doesn't have to be used in all the application, because not all of it will use it.-->
+    @yield('css')
+
+    <!-- Bootstrap CSS-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <title>CRUD</title>
@@ -12,10 +17,11 @@
     <h1 class="bg-primary text-white text-center">CRUD con laravel 8 y bootstrap v5</h1>
 
     <div class="container">
-        @yield('contenido');
+        @yield('contenido')
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
+    @yield('js')  
   </body>
 </html>
